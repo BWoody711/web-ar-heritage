cf5bfa139976ff7d3fca7244721c86eca20b350c
Ben Woodward <66846892+BWoody711@users.noreply.github.com>
2024-02-25 11:44:12 -0500

    Initial commit

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { page } from '$app/stores'
+    const slugArr = $page.params.slug.split('/')
+</script>
+
+<body>
+    <h1>Docs home page</h1>
+    {#if slugArr.length === 1}
+        <h1>Length is 1</h1>
+    {:else if slugArr.length === 2}
+        <h1>Length is 2</h1>
+    {/if}
+</body>
---
+++ src/routes/items/[itemId]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { page } from '$app/stores'
+    const itemId = $page.params.itemId
+</script>
+
+<body>
+    <h1>Item {itemId}</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</body>
