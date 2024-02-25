72f1596d9844e401ac93b62e9ff15823716a7b49
Ben Woodward <bwmanh@gmail.com>
2024-02-25 17:52:14 -0500

    Updated build

---
+++ build/_app/immutable/entry/start.os6mpv-j.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.bKF4LObe.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
     kit: {
         adapter: adapter({ fallback: '404.html' }),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+        }
     },
 };
 
