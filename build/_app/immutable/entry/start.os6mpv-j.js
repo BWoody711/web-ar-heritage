5bb1d76d8756a59b85c9122fe5da2d579f72c613
Ben Woodward <bwmanh@gmail.com>
2024-02-25 18:02:03 -0500

    Yaml

--- build/_app/immutable/entry/start.os6mpv-j.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.bKF4LObe.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ const config = {
     kit: {
         adapter: adapter({ fallback: '404.html' }),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
         }
     },
 };
