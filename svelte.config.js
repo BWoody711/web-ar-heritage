8d7f68018f475d63b10ecff4fe84d4151b4eb305
Ben Woodward <bwmanh@gmail.com>
2024-02-25 18:06:49 -0500

    Edited GitIgnore

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ const config = {
     kit: {
         adapter: adapter({ fallback: '404.html' }),
 		paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+            base: process.argv.includes('dev') ? '/web-ar-heritage' : process.env.BASE_PATH
         }
     },
 };
